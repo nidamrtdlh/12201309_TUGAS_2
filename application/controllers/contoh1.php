87802a69d29d53258e0a8126cb6983f35ddc4990
nidamrtdlh <nidamrtdlh16@gmail.com>
2021-09-24 19:16:37 +0700

    menambahkan framework

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,14 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>;
+        echo "Nama saya Aldi Mardian
+            Saya tinggal di Jalan Gunung Subang
+            Musik yang saya sukai adalah
+            Kpop
+            Nomor handphone 085721724977";
+    }
+}
+?>
