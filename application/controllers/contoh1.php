e7f9241d6c1a2dca830233e02c7b86be249a7efc
NIDA MURTADOILAH <90882972+nidamrtdlh@users.noreply.github.com>
2021-09-24 19:26:49 +0700

    Update contoh1.php

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -4,7 +4,7 @@ class contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>;
-        echo "Nama saya Aldi Mardian
+        echo "Nama saya Nida Murtadoilah
             Saya tinggal di Jalan Gunung Subang
             Musik yang saya sukai adalah
             Kpop
